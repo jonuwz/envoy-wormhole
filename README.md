37e6f25e0e107b74a174a86c67aa50c85268c529
John Hughes <john@make.run>
2021-05-19 01:23:21 +0100

    cleanup comit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # envoy-wormhole
 example setup to tunnel traffic
 
+exposed on localhost:10000, will connect to whatever your backends are in envoy-backend
+
 TCP -> envoy -> mTLS -> envoy -> backend
+
+TODO:
+LB, healthchecks etc etc
--- create_certs.sh
+++ create_certs.sh
@@ -2,39 +2,22 @@
 
 mkdir -p certs
 openssl genrsa -out certs/ca.key 4096
-
 openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 1024 -subj "/CN=wormhole" -out certs/ca.crt
 
 openssl genrsa -out certs/proxy.key 2048
 
-openssl req -new -sha256 \
-     -key certs/proxy.key \
-     -subj "/C=US/ST=CA/O=tunnels/CN=proxy-splunk-frontend.home.local" \
-     -out certs/proxy-splunk-frontend.home.local.csr
+for i in frontend backend;do
 
-openssl req -new -sha256 \
-     -key certs/proxy.key \
-     -subj "/C=US/ST=CA/O=tunnels/CN=proxy-splunk-backend.home.local" \
-     -out certs/proxy-splunk-backend.home.local.csr
+  openssl req -new -sha256 -key certs/proxy.key -subj "/C=US/ST=CA/O=wormhole/CN=proxy-splunk-${i}.home.local" -out "certs/proxy-splunk-${i}.home.local.csr"
 
-openssl x509 -req \
-     -in certs/proxy-splunk-frontend.home.local.csr \
+  openssl x509 -req \
+     -in certs/proxy-splunk-${i}.home.local.csr \
      -CA certs/ca.crt \
      -CAkey certs/ca.key \
      -CAcreateserial \
-     -extfile <(printf "subjectAltName=DNS:proxy-splunk-frontend.home.local") \
-     -out certs/proxy-splunk-frontend.home.local.crt \
+     -extfile <(printf "subjectAltName=DNS:proxy-splunk-${i}.home.local") \
+     -out certs/proxy-splunk-${i}.home.local.crt \
      -days 500 \
      -sha256
 
-openssl x509 -req \
-     -in certs/proxy-splunk-backend.home.local.csr \
-     -CA certs/ca.crt \
-     -CAkey certs/ca.key \
-     -CAcreateserial \
-     -extfile <(printf "subjectAltName=DNS:proxy-splunk-backend.home.local") \
-     -out certs/proxy-splunk-backend.home.local.crt \
-     -days 500 \
-     -sha256
-
-
+done
